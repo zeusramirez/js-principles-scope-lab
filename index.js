10bb041c9e29ee1adac3e9a21d8094b8cb4d8f64
zeus <zeus.ramirez347@gmail.com>
2021-05-23 19:06:36 -0400

    commit tree 1c280c412565d318c95e7cc37f465b8eb3a80fbb

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'bobs brother'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'bob'
+}
